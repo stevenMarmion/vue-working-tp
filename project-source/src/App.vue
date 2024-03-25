7b4748bf9a57b5e80f74735525def637b6450681
marmionsteven8 <marmionsteven8@gmail.com>
2024-03-25 11:01:21 +0100

    Ajout du début de la modifications des questionnaires

--- project-source/src/App.vue
+++ project-source/src/App.vue
@@ -1,29 +1,24 @@
 <script>
 // Options API
 
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import QuestionnaireUpdate from './components/QuestionnaireUpdate.vue';
 
 export default {
   data() {
-    return [
-      questionnaires = [
-        {  1 : {'nom' : 'questionnaire1',
-                'questions' : [1,2,3]}
-        },
-        {  2 : {'nom' : 'questionnaire2',
-                'questions' : [4,5,6]}
-        }
+    return {
+      questionnaires : [
+        { id : 1, titre : 'questionnaire1', questions : [1,2,3] },
+        { id : 2, titre : 'questionnaire2', questions : [4,5,6] },
       ],
-      questions = [
-        {  1 : 'question1'  },
-        {  2 : 'question2'  },
-        {  3 : 'question3'  },
-        {  4 : 'question4'  },
-        {  5 : 'question5'  },
-        {  6 : 'question6'  }
+      questions : [
+        {  id : 1, texte : 'question1'  },
+        {  id : 2, texte : 'question2'  },
+        {  id : 3, texte : 'question3'  },
+        {  id : 4, texte : 'question4'  },
+        {  id : 5, texte : 'question5'  },
+        {  id : 6, texte : 'question6'  }
       ]
-    ]
+    }
   },
   methods : {
     ajouter_questionnaire : function(questionnaire) {
@@ -38,21 +33,18 @@ export default {
     supprimer_question : function(id_question) {
       delete this.questions[id_question];
     },
-  }
+  },
+  components: {QuestionnaireUpdate}
 }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>On modifie question</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <QuestionnaireUpdate v-for="ques of questionnaires" :questionnaire = "ques"></QuestionnaireUpdate>
   </main>
 </template>
 
